7a9f2a2511d9f4ad6d475134069ec222f89b0b66
Kaito Hirose <kahirose@c1r26s3.42tokyo.jp>
2022-04-17 13:21:12 +0900

    264

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -3,3 +3,4 @@ ALTER TABLE record_last_access ADD INDEX index_rla (record_id, user_id);
 ALTER TABLE record_comment ADD INDEX index_rc (linked_record_id);
 ALTER TABLE record ADD INDEX index_record (created_by, status);
 ALTER TABLE record_item_file ADD INDEX index_rif (linked_record_id, item_id);
+ALTER TABLE record ADD INDEX index_rca (category_id, application_group);
