a6e1c15f20a2a79827a3f34fbd5c85bd0e62ce36
Kaito Hirose <kahirose@c1r26s3.42tokyo.jp>
2022-04-17 11:26:04 +0900

    score 163

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,3 +1,4 @@
 ALTER TABLE session ADD INDEX index_yada (value);
 ALTER TABLE record_last_access ADD INDEX index_rla (record_id, user_id);
 ALTER TABLE record_comment ADD INDEX index_rc (linked_record_id);
+ALTER TABLE record ADD INDEX index_record (created_by, status);
